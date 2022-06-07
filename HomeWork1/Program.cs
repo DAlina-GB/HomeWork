2145c7d0a8fe1d9d68b811f04036b2e5d0af13fd
Alina Dainakova <alinoskar@gmail.com>
2022-06-07 18:31:29 +0300

    Задача 1

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*int num1, num2;
+Console.Write ("Input first integer number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Input second integer number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+
+{
+	Console.WriteLine ("max " + num1 );
+}
+ else
+{  
+	Console.WriteLine ("max " + num2 );
+}
+*/
