3de10c399fcde1ad91bbc6669d44fc3222e0480e
Alina Dainakova <alinoskar@gmail.com>
2022-06-08 13:06:38 +0300

    Задача2-001

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -13,10 +13,20 @@ if (num1 > num2)
 	Console.WriteLine ("max " + num2 );
 }
 */
-int num1, num2, num3;
-Console.Write ("Input first integer number: ");
+ int num1, num2, num3; 
+
+Console.Write("Input first integer number: ");
 num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write ("Input second integer number: ");
+Console.Write("Input second integer number: ");
 num2 = Convert.ToInt32(Console.ReadLine());
-Console.Write ("Input third integer number: ");
+Console.Write("Input third integer number: ");
 num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+If(num1 > max) max = num1;
+If(num2 > max) max = num2;
+If(num3 > max) max = num3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
