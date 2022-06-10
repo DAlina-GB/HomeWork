b171f0ce506295a9e70edc4d4236e948416b411c
Alina Dainakova <alinoskar@gmail.com>
2022-06-11 00:15:23 +0300

    Последнии штрихи

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,4 +1,5 @@
-﻿/*int num1, num2;
+﻿//Задача №1
+/*int num1, num2;
 Console.Write ("Input first integer number: ");
 num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write ("Input second integer number: ");
@@ -13,6 +14,8 @@ if (num1 > num2)
 	Console.WriteLine ("max " + num2 );
 }
 */
+
+//Задача №2
 /*
 int num1, num2, num3; 
 Console.Write("Input first integer number: ");
@@ -31,6 +34,8 @@ if(num3 > max) max = num3;
 Console.Write("max = ");
 Console.WriteLine(max);
 */
+
+//Задача №3
 /*int num;
 Console.Write ("Input integer number: ");
 num = Convert.ToInt32(Console.ReadLine());
@@ -43,9 +48,11 @@ else
 	Console.WriteLine ("No");
 }
 */
-int N;
+//Задача №4
+/*int N;
 Console.Write ("Input integer number: ");
 N = Convert.ToInt32(Console.ReadLine());
 for (int num1 = 2; num1 < N; num1 ++)       
 if (num1%2==0)            
 Console.Write(num1+" ");
+*/
