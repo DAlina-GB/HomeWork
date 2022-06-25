68366749027ae7b1b00982922478ad21cfc47c53
Alina Dainakova <alinoskar@gmail.com>
2022-06-25 13:24:46 +0300

    готова только одна задача

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача №1
+//Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)    2, 4 -> 16
+/*
+int Exponentiation(int num1, int num2)
+{
+  int result = 1;
+  for(int i=1; i <= num2; i++)
+  {
+    result *= num1;
+  }
+
+    return result;
+}
+
+  Console.Write ("Input number A: ");
+  int num1 = Convert.ToInt32(Console.ReadLine());
+  Console.Write ("Input number B: ");
+  int num2 = Convert.ToInt32(Console.ReadLine());
+
+  int res = Exponentiation (num1,num2);
+  Console.WriteLine($"Answer: {res}");
+*/
+
+//Задача №2
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19, 3, 44, 3 -> [1, 2, 5, 7, 19, 3, 44, 3
+
