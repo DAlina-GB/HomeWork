548b7f33303c9fcb70fde4d3426f688ac39da632
Alina Dainakova <alinoskar@gmail.com>
2022-07-01 14:46:36 +0300

    done

---
+++ Seminar_8/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задайте двумерный массив. Напишите программу, 
+//которая поменяет местами первую и последнюю строку массива.
+int[,] CreareTwoDimArray(int m, int n, int min, int max)
+{
+    int[,] array = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            array[i, j] = new Random().Next(min, max + 1);
+    return array;
+}
+
+void ShowTwoDimArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i, j] + " ");
+    
+    Console.WriteLine();
+    }
+}
+int[,] ChangeLines(int[,] array, int strIndex1, int strIndex2)
+{
+    int temp;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        temp = array[strIndex1, j];
+        array[strIndex1, j] = array[strIndex2, j];
+        array[strIndex2, j] = temp;
+    }
+    return array;
+}
+
+int[,] myArray = CreareTwoDimArray(4, 5, 1, 9);
+ShowTwoDimArray(myArray);
+myArray = ChangeLines(myArray, 0, myArray.GetLength(0) - 1); //myArray.GetLength(0) - 1 обращение к последней строчке массива
+Console.WriteLine();
+ShowTwoDimArray(myArray);
